2bb913f72e674cc54a134ef35fd1e3e8bfd5aa6f
sigged <sderdeyn@gmail.com>
2020-11-09 16:51:57 +0100

    Add project files.

---
+++ Howest.Prog.Cia.CurrencySwapper.Web/Models/ConvertViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Howest.Prog.Cia.CurrencySwapper.Web.Models
+{
+    public class ConvertViewModel
+    {
+        public string SourceCurrency { get; set; }
+
+        public string TargetCurrency { get; set; }
+
+        public double Amount { get; set; }
+
+        public double ConvertedAmount { get; set; }
+
+        public bool ShowResult { get; set; }
+    }
+}
