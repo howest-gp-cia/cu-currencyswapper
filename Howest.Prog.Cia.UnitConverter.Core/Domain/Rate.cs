ae7929f8e04a42a898090afc2398133aa447f9bc
sigged <sderdeyn@gmail.com>
2020-11-10 10:06:02 +0100

    Add Rate entity

---
+++ Howest.Prog.Cia.UnitConverter.Core/Domain/Rate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Howest.Prog.Cia.UnitConverter.Core.Domain
+{
+    public class Rate
+    {
+        public double ExchangeRate { get; set; }
+
+        public string FromCurrency { get; set; }
+
+        public string ToCurrency { get; set; }
+    }
+}
---
+++ Howest.Prog.Cia.UnitConverter.Core/Infrastructure/IRateService.cs
@@ -0,0 +1,15 @@
+﻿using Howest.Prog.Cia.UnitConverter.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.Prog.Cia.UnitConverter.Core.Infrastructure
+{
+    public interface IRateService
+    {
+        Task<Rate> GetRate(string fromCurrency, string toCurrency);
+
+        Task<IEnumerable<string>> GetSupportedCurrencies();
+    }
+}
